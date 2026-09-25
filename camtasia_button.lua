91c517ab240cf7af83154d11ff996a14ea20fd56
espritmarin21 <espritmarin2017@gmail.com>
2026-09-25 16:05:44 +0100

    Add camtasia_button.lua (Camtasia button On Click)

---
+++ camtasia_button.lua
@@ -0,0 +1,9 @@
+pos = Window.GetPos(Application.GetWndHandle());
+old_x = pos.X;
+old_y = pos.Y;
+--Button.SetVisible("icamtasia", false);
+Button.SetVisible("all", false);
+
+
+--Button.SetPos("camlanguage", 133, 250);
+Page.Jump("camtasia");
